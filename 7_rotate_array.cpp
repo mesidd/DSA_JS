51677f7b4077e490a4bbe8b0d4c8000e69659db1
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2025-03-05 18:52:19 +0530

    Rotate array by one clockwise

---
+++ 7_rotate_array.cpp
@@ -0,0 +1,11 @@
+   void rotate(vector<int> &arr) {
+        int n = arr.size();
+        if ( n <= 1) return;
+        
+        int last = arr[n-1];
+        
+        for ( int i = n-1 ; i> 0; i--){
+        arr[i] = arr[i-1];
+        }
+        arr[0] = last;
+    }
