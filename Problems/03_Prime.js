31466f4193810bd17fbc684c94f92bd6d3138b93
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2025-04-13 01:01:25 +0530

    prime number

---
+++ Problems/03_Prime.js
@@ -0,0 +1,10 @@
+function Prime(n){
+  for ( let i = 2 ; i < n ; i++ ){
+    if ( n % i === 0 ) return "Non Prime"
+  }
+  return "Prime"
+}
+
+console.log(Prime(3));
+console.log(Prime(12));
+console.log(Prime(99));
