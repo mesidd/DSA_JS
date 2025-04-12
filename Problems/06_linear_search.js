ca67df96472512a3588816d0ce5974363c8fa96b
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2025-04-13 03:09:59 +0530

    linear search

---
+++ Problems/06_linear_search.js
@@ -0,0 +1,12 @@
+function linearSearch( arr, value){
+  for( let i = 0; i < arr.length; i++){
+    if(arr[i] === value){
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(linearSearch([1,2,3,4,5],2)) // 1
+console.log(linearSearch([1,2,3,4,5],11)) // -1
+console.log(linearSearch([1,2,3,4,5],3)) // 2
