bc63e08de471a0be1337147552da3baf195c69c8
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2025-04-16 12:28:47 +0530

    two sets cartesian product

--- Problems/Miscellaneous/01_Cartesian_Product.js
+++ Problems/Miscellaneous/01_Cartesian_Product.js
@@ -1,12 +1,15 @@
 function cartesianProduct(arr1,arr2){
   const arr = []
   for ( let i = 0 ; i < arr1.length ; i++ ){
-    let temp = [];
+    // let temp = [];
     for ( let j = 0 ; j < arr2.length ; j++ ){
-      temp.push(arr1[i],arr2[j])
-      arr.push(temp);
-      temp = [];
+      // temp.push(arr1[i],arr2[j])
+      // arr.push(temp);
+      // temp = [];
+      arr.push([arr[i],arr[j]]);
     }
   }
   return arr;
 }
+
+
