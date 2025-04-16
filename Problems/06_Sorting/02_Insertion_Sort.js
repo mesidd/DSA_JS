66a9cb1993ecd66169201232b12ac9bce9719145
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2025-04-16 09:28:42 +0530

    insertion sorting

---
+++ Problems/06_Sorting/02_Insertion_Sort.js
@@ -0,0 +1,13 @@
+function insertionSort(arr){
+  for(let i = 1; i < arr.length; i++){
+    let num = arr[i];
+    let j = i - 1;
+
+    while( j >= 0 && arr[j] > num ){
+      arr[j+1] = arr[j];
+      j--;
+    }
+    arr[j+1] = num;
+  }
+  return arr;
+}
