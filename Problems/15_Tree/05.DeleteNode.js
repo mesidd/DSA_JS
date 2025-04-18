1b3793f57cb4c422466ca131d4ac330c13120a02
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2025-04-18 14:55:42 +0530

    delete node

--- Problems/15_Tree/05.DeleteNode.js
+++ Problems/15_Tree/05.DeleteNode.js
@@ -3,21 +3,31 @@ delete(value){
 }
 
 deleteNode(root, value){
+  
   if(root === null) return root;
+  
   if(value < root.value){
     root.left = this.deleteNode(root.left, value);
-  } else if (value > root.value){
+  } 
+  else if (value > root.value){
     root.right = this.deleteNode(root.right, value);
-  } else{
+  } 
+  else{
+    
     if(!root.left && !root.right){
       this.root = null;
-    } else if(root.left === null){
-      this.root = this.deleteNode(root.right, value);
-  } else if(root.right === null){
-      this.root = this.deleteNode(root.left, value);
-  } else{
+    } 
+    else if(root.left === null){
+      return root.right;
+  } 
+    else if(root.right === null){
+      return root.left;
+  } 
+    else{
       let minNode = this.minNode(root.right);
       root.value = minNode;
       root.right = this.deleteNode(root.right, value);
   }
+}
+  return root;
 }
