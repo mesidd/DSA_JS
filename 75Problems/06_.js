643a99532acaf8ecffc721280626293f121c55d5
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2025-04-21 09:22:11 +0530

    counting bit 1 in a number

--- 75Problems/06_.js
+++ 75Problems/06_.js
@@ -1,7 +1,7 @@
 class Solution {
     bitsOne(n) {
-        let count = 0;
-        let compare = n;
+    let count = 0;
+    let compare = n;
         while( compare > 0 ){
             count += compare & 1;
             compare >>= 1;
