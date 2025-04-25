79e85b8694f94546f9f29d6ea8e375877b3d79fa
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2025-04-26 03:39:03 +0530

    Anagram Check

---
+++ 75Problems/10_.js
@@ -0,0 +1,8 @@
+class Solution {
+    isAnagram(s, t) {
+        if(s.length !== t.length ) return false;
+        let sSort = s.split("").sort().join();
+        let tSort = t.split("").sort().join();
+        return (sSort === tSort);
+    }
+}
